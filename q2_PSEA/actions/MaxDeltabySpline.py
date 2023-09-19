015392ea8fa28f0c8bcee92f7fa71d3e54ae076d
Num0Programmer <asajh4@gmail.com>
2023-09-19 15:15:36 -0700

    Added link to aforementioned tutorial and removed unneeded print statements, as well as added a comment to explain why "knot_numbers" variable is equal to 5.

--- q2_PSEA/actions/MaxDeltabySpline.py
+++ q2_PSEA/actions/MaxDeltabySpline.py
@@ -1,6 +1,4 @@
-# resources: https://rpy2.github.io/doc/latest/html/generated_rst/pandas.html
-#            https://stackoverflow.com/questions/51167555/how-to-predict-using-rpy2
-#            https://scikit-learn.org/stable/auto_examples/linear_model/plot_polynomial_interpolation.html
+# resources: https://www.datatechnotes.com/2021/11/scattered-data-spline-fitting-example.html
 
 import numpy as np
 import pandas as pd
@@ -12,11 +10,10 @@ from scipy import interpolate
 def max_delta_by_spline(timepoint1, timepoint2, indata: pd.DataFrame) -> list:
     # specific code for smooth spline
     # TODO: probably gets wrapped in a dedicated function called "spline" - most likely
+    knot_numbers = 5 # default value from tutorial
     y = indata.loc[:, timepoint2].to_numpy()
     x = range(0, len(y))
-    knot_numbers = 5
     x_new = np.linspace(0, 1, knot_numbers+2)[1:-1]
-    print("x_new: %s" % x_new)
 
     # giving column at timepoint1 because R code passes indata[[timepoint1]] to predict()
     # tutorial x is indata[[timepoint1]] at the moment
@@ -25,9 +22,7 @@ def max_delta_by_spline(timepoint1, timepoint2, indata: pd.DataFrame) -> list:
     # smoothing condition from smooth.spline() in original R code
     t, c, k = interpolate.splrep(x, y, t=q_knots, s=0.788458)
     yfit = interpolate.BSpline(t, c, k)(indata[[timepoint1]])
-    print("yfit: %s" % yfit)
-    # print("indata col=timepoint1: %s" % len(indata.loc[:, timepoint1].to_numpy()))
-    # print("indata col=timepoint2: %s" % len(indata.loc[:, timepoint2].to_numpy()))
+    print("yfit: %s" % yfit) # TODO: remove when finished troubleshooting for expected test values
 
     # code which is the general idea of max delta by splining
     maxZ = np.apply_over_axes(np.max, indata[[timepoint1, timepoint2]], 1)
