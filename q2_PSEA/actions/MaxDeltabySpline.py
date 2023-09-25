41dd93c000736ce8e2bed1a5450bc7b37355c08c
Num0Programmer <asajh4@gmail.com>
2023-09-25 15:30:57 -0700

    Finished a whole initial implementation of MaxDeltabySpline.R into Python. At the moment, there appears to be no equivalent implementaton of R's smooth spline code in Python, so the values might be off; in addition, there may be some mistakes on the my side which are affecting the values and will need to be worked out later. At least now, this code is working and putting out something that can be used by runPSEA.py code.

--- q2_PSEA/actions/MaxDeltabySpline.py
+++ q2_PSEA/actions/MaxDeltabySpline.py
@@ -8,19 +8,52 @@ from scipy import interpolate
 
 
 def max_delta_by_spline(timepoint1, timepoint2, indata: pd.DataFrame) -> list:
-    # 5 is the default value from above tutorial
-    print("yfits: %s" % spline(5, indata.loc[:, timepoint2].to_numpy()))
+    """
 
-    # code which is the general idea of max delta by splining
-    maxZ = np.apply_over_axes(np.max, indata[[timepoint1, timepoint2]], 1)
-    return list
+    Parameters
+    ----------
+    timepoint1 : str
+
+    timepoint2 : str
+
+    indata : pd.DataFrame
+
+    Returns
+    -------
+    """
+    maxZ = np.apply_over_axes(np.max, indata.loc[:, [timepoint1, timepoint2]], 1)
+
+    # perform smoothing spline prediction
+    y = indata.loc[:, timepoint2].to_numpy()
+    x = indata.loc[:, timepoint1].to_numpy()
+    # tentative magic number 5 (knots) came from tutorial linked above
+    smooth_spline = spline(5, y)
+    deltaZ = y - smooth_spline(x)
+
+    # convert maxZ and deltaZ to pandas Series - allows for association of
+    # values with peptides
+    maxZ = pd.Series(data=[num for num in maxZ], index=indata.index)
+    deltaZ = pd.Series(data=deltaZ, index=indata.index)
+    
+    return [maxZ, deltaZ, smooth_spline(x), smooth_spline(y)]
 
 
 def spline(knots, y):
+    """<description>
+
+    Parameters
+    ----------
+    knots : int
+
+    y : float array
+
+    Returns
+    -------
+    BSpline
+    """
     x = range(0, len(y))
     x_new = np.linspace(0, 1, knots+2)[1:-1]
     q_knots = np.quantile(x, x_new)
-    # smoothing condition (s) from smooth.spline() in original R code
+    # smoothing condition `s` from smooth.spline() in original R code
     t, c, k = interpolate.splrep(x, y, t=q_knots, s=0.788458)
-    yfit = interpolate.BSpline(t, c, k)(x)
-    return yfit
+    return interpolate.BSpline(t, c, k)
