1515b27027a9bf8b112f7e3ade984503542e6e18
Num0Programmer <asajh4@gmail.com>
2024-02-06 09:40:14 -0700

    Added code insto install_r_packages.R dedicated to installing rigraph; however, there still are dependencies which the user will need to seek out if Anaconda does not already install them. Added a note that Anaconda does have a clusterProfiler package; but due to conflicts with dependencies, the user may want to have a dedicated environment.

--- install_r_packages.R
+++ install_r_packages.R
@@ -1,3 +1,16 @@
+# Please note there is also a package via Anaconda: https://anaconda.org/bioconda/bioconductor-clusterprofiler
+# Additionally, please be aware you may want to have a dedicated environment if
+# you install via Anaconda because of conflicts with various dependencies
+
+# Install igraph: https://github.com/igraph/rigraph
+options(
+  repos = c(
+    igraph = 'https://igraph.r-universe.dev',
+    CRAN = 'https://cloud.r-project.org'
+  )
+)
+install.packages('igraph')
+
 # Installation instructions for Bioconductor: https://cran.r-project.org/web/packages/BiocManager/vignettes/BiocManager.html#installing-biocmanager
 install.packages("BiocManager", repos = "https://cloud.r-project.org")
 
