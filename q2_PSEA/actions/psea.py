33310b14f32a567e9366671ec48bd6720517ff35
SeanGolez <seagol1010@gmail.com>
2024-05-17 12:04:26 -0700

    Implement multiprocessing
    For a small dataset, these changes seemed to cut the times down in half. There is still room for improvements.

--- q2_PSEA/actions/psea.py
+++ q2_PSEA/actions/psea.py
@@ -8,6 +8,7 @@ import q2_PSEA.utils as utils
 import tempfile
 
 import time
+import concurrent.futures
 
 from math import isnan, log, pow
 from rpy2.robjects import pandas2ri
@@ -40,7 +41,7 @@ def make_psea_table(
         iter_tables_dir="./psea_iter_tables_outdir",
         get_iter_tables=False
 ):    
-    start_time = time.perf_couter()
+    start_time = time.perf_counter()
 
     volcano = ctx.get_action("ps-plot", "volcano")
     zscatter = ctx.get_action("ps-plot", "zscatter")
@@ -131,6 +132,8 @@ def make_psea_table(
                 y = data_sorted.loc[:, pair[1]].to_numpy()
 
                 # TODO: optimize with a dictionary, if possible
+                # Note: changes here should be added to iterative function aswell
+                # TODO: possibly make this part a function to reduce redundant code
                 if spline_type == "py-smooth":
                     yfit = splines.smooth_spline(x, y)
                 elif spline_type == "cubic":
@@ -212,7 +215,7 @@ def make_psea_table(
                 titles=titles
             )
 
-    end_time = time.perf_couter()
+    end_time = time.perf_counter()
 
     print(f"Finished in {round(end_time-start_time, 2)} seconds")
     
@@ -301,95 +304,147 @@ def run_iterative_peptide_analysis(
             if not os.path.exists(iter_out_dir):
                 os.mkdir(iter_out_dir)
 
-        if not dof:
-            dof = ro.NULL
+        # -------------------------------
+        # note: rpy2 is not compatible with threading
+        with concurrent.futures.ProcessPoolExecutor() as executor:
+            pair_futures = [executor.submit(run_iterative_process_single_pair,
+                            pair, 
+                            tested_species_dict[pair], 
+                            pair_gmt_dict[pair], 
+                            processed_scores,
+                            species_taxa_file,
+                            threshold,
+                            permutation_num,
+                            min_size,
+                            max_size,
+                            spline_type,
+                            degree,
+                            dof,
+                            p_val_thresh,
+                            es_thresh,
+                            peptide_sets_out_dir,
+                            get_iter_tables
+                            ) for pair in pairs if sig_species_found_dict[pair]]
 
-        for pair in pairs:
-            if( sig_species_found_dict[ pair ] ):
-                print(f"Working on pair ({pair[0]}, {pair[1]})...")
+            # concurrent.futures.wait(pair_futures, timeout=None, return_when=concurrent.futures.ALL_COMPLETED)
 
-                sig_species_found_dict[ pair ] = False
-
-                # create gmt file for this pair (filtered gmt from prev iteration)
-                pair_sets_filename_dict[ pair ] = f"{peptide_sets_out_dir}/{pair[0]}_{pair[1]}".replace(".","-") + ".gmt"
-
-                write_gmt_from_dict(pair_sets_filename_dict[ pair ], pair_gmt_dict[ pair ])
-
-                processed_scores, peptide_sets = utils.remove_peptides(
-                    processed_scores, pair_sets_filename_dict[ pair ]
-                )
-
-                data_sorted = processed_scores.loc[:, pair].sort_values(by=pair[0])
-                x = data_sorted.loc[:, pair[0]].to_numpy()
-                y = data_sorted.loc[:, pair[1]].to_numpy()
-
-                # TODO: optimize with a dictionary, if possible
-                if spline_type == "py-smooth":
-                    yfit = splines.smooth_spline(x, y)
-                elif spline_type == "cubic":
-                    yfit = splines.R_SPLINES.cubic_spline(x, y, degree, dof)
-                else:
-                    yfit = splines.R_SPLINES.smooth_spline(x, y)
-
-                maxZ = np.apply_over_axes(
-                    np.max,
-                    processed_scores.loc[:, pair],
-                    1
-                )
-                maxZ = pd.Series(
-                    data=[num for elem in maxZ for num in elem],
-                    index=processed_scores.index
-                )
-                deltaZ = pd.Series(
-                    data=y - yfit, index=data_sorted.index
-                ).sort_index()
-
-                table = INTERNAL.psea(
-                    maxZ,
-                    deltaZ,
-                    peptide_sets,
-                    species_taxa_file,
-                    threshold,
-                    permutation_num,
-                    min_size,
-                    max_size
-                )
-                with (ro.default_converter + pandas2ri.converter).context():
-                    table = ro.conversion.get_conversion().rpy2py(table)
-
-                # sort the table by ascending p-value (lowest on top)
-                table.sort_values(by=["pvalue"], ascending=True)
-
-                if get_iter_tables:
-                    table.to_csv(f"{iter_out_dir}/{pair}.tsv", sep="\t")
-
-                # iterate through each row
-                for index, row in table.iterrows():
-
-                    # test for significant species that has not already been used for this pair
-                    if row["pvalue"] < p_val_thresh and np.absolute(row["enrichmentScore"]) > es_thresh \
-                                                and row["ID"] not in tested_species_dict[pair]:
-
-                        print(f"Found {row['species_name']} in {pair} to be significant")
-
-                        # set sig species found to true for this pair
-                        sig_species_found_dict[ pair ] = True
-
-                        tested_species_dict[pair].add(row["ID"])
-
-                        # take out all tested peptides from peptide set in gmt for all other species in the gmt
-                        all_tested_peps = set(row["all_tested_peptides"].split("/"))
-                        for gmt_species in pair_gmt_dict[ pair ].keys():
-                            if gmt_species != row['ID']:
-                                pair_gmt_dict[pair][ gmt_species ] = pair_gmt_dict[pair][ gmt_species ] - all_tested_peps
-
-                        # only get top significant species
-                        break
+            for future in concurrent.futures.as_completed(pair_futures):
+                res_tup = future.result()
+                pair = res_tup[4]
+                sig_species_found_dict[pair] = res_tup[0]
+                pair_sets_filename_dict[pair] = res_tup[1]
+                tested_species_dict[pair] = res_tup[2]
+                pair_gmt_dict[pair]= res_tup[3]        
+        # -------------------------------
 
         iteration_num += 1
 
     print("End of Iterative Peptide Analysis\n")
     return pair_sets_filename_dict
+
+
+def run_iterative_process_single_pair(
+    pair, 
+    tested_species, 
+    gmt_dict, 
+    processed_scores,
+    species_taxa_file,
+    threshold,
+    permutation_num,
+    min_size,
+    max_size,
+    spline_type,
+    degree,
+    dof,
+    p_val_thresh,
+    es_thresh,
+    peptide_sets_out_dir,
+    get_iter_tables
+    ):
+    if not dof:
+        dof = ro.NULL
+
+    print(f"Working on pair ({pair[0]}, {pair[1]})...")
+
+    sig_species_found = False
+
+    # create gmt file for this pair (filtered gmt from prev iteration)
+    pair_sets_filename = f"{peptide_sets_out_dir}/{pair[0]}_{pair[1]}".replace(".","-") + ".gmt"
+
+    write_gmt_from_dict(pair_sets_filename, gmt_dict)
+
+    processed_scores, peptide_sets = utils.remove_peptides(
+        processed_scores, pair_sets_filename
+    )
+
+    data_sorted = processed_scores.loc[:, pair].sort_values(by=pair[0])
+    x = data_sorted.loc[:, pair[0]].to_numpy()
+    y = data_sorted.loc[:, pair[1]].to_numpy()
+
+    # TODO: optimize with a dictionary, if possible
+    if spline_type == "py-smooth":
+        yfit = splines.smooth_spline(x, y)
+    elif spline_type == "cubic":
+        yfit = splines.R_SPLINES.cubic_spline(x, y, degree, dof)
+    else:
+        yfit = splines.R_SPLINES.smooth_spline(x, y)
+
+    maxZ = np.apply_over_axes(
+        np.max,
+        processed_scores.loc[:, pair],
+        1
+    )
+    maxZ = pd.Series(
+        data=[num for elem in maxZ for num in elem],
+        index=processed_scores.index
+    )
+    deltaZ = pd.Series(
+        data=y - yfit, index=data_sorted.index
+    ).sort_index()
+
+    table = INTERNAL.psea(
+        maxZ,
+        deltaZ,
+        peptide_sets,
+        species_taxa_file,
+        threshold,
+        permutation_num,
+        min_size,
+        max_size
+    )
+    with (ro.default_converter + pandas2ri.converter).context():
+        table = ro.conversion.get_conversion().rpy2py(table)
+
+    # sort the table by ascending p-value (lowest on top)
+    table.sort_values(by=["pvalue"], ascending=True)
+
+    if get_iter_tables:
+        table.to_csv(f"{iter_out_dir}/{pair}.tsv", sep="\t")
+
+    # iterate through each row
+    for index, row in table.iterrows():
+
+        # test for significant species that has not already been used for this pair
+        if row["pvalue"] < p_val_thresh and np.absolute(row["enrichmentScore"]) > es_thresh \
+                                    and row["ID"] not in tested_species:
+
+            print(f"Found {row['species_name']} in {pair} to be significant")
+
+            # set sig species found to true for this pair
+            sig_species_found = True
+
+            tested_species.add(row["ID"])
+
+            # take out all tested peptides from peptide set in gmt for all other species in the gmt
+            all_tested_peps = set(row["all_tested_peptides"].split("/"))
+            for gmt_species in gmt_dict.keys():
+                if gmt_species != row['ID']:
+                    gmt_dict[ gmt_species ] = gmt_dict[ gmt_species ] - all_tested_peps
+
+            # only get top significant species
+            break
+
+    return (sig_species_found, pair_sets_filename, tested_species, gmt_dict, pair)
 
 
 def write_gmt_from_dict(outfile_name, gmt_dict)->None:
